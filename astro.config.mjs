baada730646f096ad84afd6354e2271edcb949da
callum <ccallum.wong@gmail.com>
2025-04-29 01:19:17 +1000

    migrate everything from old blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import rehypeFigureTitle from "rehype-figure-title";
+
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  markdown: {
+    rehypePlugins: [rehypeFigureTitle]
+  },
+
+  integrations: [mdx()]
 });
