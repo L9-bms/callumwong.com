fa1c0bb610f4f485174b18709cca658745bee76f
Callum <mail@callumwong.com>
2026-04-16 01:42:45 +1000

    renovate

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import rehypeFigureTitle from "rehype-figure-title";
+import rehypeGithubAlert from "rehype-github-alert";
 
 import mdx from "@astrojs/mdx";
 
 import vercel from "@astrojs/vercel";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,10 +18,13 @@ export default defineConfig({
   },
 
   markdown: {
-    rehypePlugins: [rehypeFigureTitle]
+    shikiConfig: {
+      theme: "min-light",
+    },
+    rehypePlugins: [rehypeFigureTitle, rehypeGithubAlert]
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
 
   adapter: vercel({
     imageService: true,
